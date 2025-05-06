790bd3b6858e722bb63a369246dad1c453899413
Spencer Dearman <dearmanspencer@gmail.com>
2025-05-06 13:02:18 -0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Spencer Dearman on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
