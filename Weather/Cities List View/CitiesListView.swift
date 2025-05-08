d871a1554ff578f3822f274ab459f379fd9936ba
Spencer Dearman <dearmanspencer@gmail.com>
2025-05-07 22:21:37 -0500

    Updating to use .searchable instead of using an overlay -- easier and cleaner solution

--- Weather/Cities List View/CitiesListView.swift
+++ Weather/Cities List View/CitiesListView.swift
@@ -6,69 +6,74 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct CitiesListView: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(DataStore.self) private var store
     let currentLocation: City?
     @Binding var selectedCity: City?
-    @State private var isSearching = false
-    @FocusState private var isFocused: Bool
+    @State private var searchText = ""
+    @State private var searchService = SearchService(completer: MKLocalSearchCompleter())
+    
+    var filteredCities: [City] {
+        if searchText.isEmpty {
+            return store.cities
+        } else {
+            return store.cities.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+    
     var body: some View {
         NavigationStack {
-            ZStack {
-                VStack {
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                        TextField("Search...", text: .constant(""))
-                            .textFieldStyle(.roundedBorder)
-                            .focused($isFocused)
-                    }
-                    .padding()
-                    .onTapGesture {
-                        withAnimation {
-                            isSearching.toggle()
-                        }
-                    }
-                    List {
-                        Group {
-                            if let currentLocation {
-                                CityRowView(city: currentLocation)
-                                    .onTapGesture {
-                                        selectedCity = currentLocation
-                                        dismiss()
-                                    }
+            List {
+                Group {
+                    if let currentLocation {
+                        CityRowView(city: currentLocation)
+                            .onTapGesture {
+                                selectedCity = currentLocation
+                                dismiss()
                             }
-                            ForEach(store.cities.sorted(using: KeyPathComparator(\.name))) { city in
-                                CityRowView(city: city)
-                                    .swipeActions {
-                                        Button(role: .destructive) {
-                                            if let index = store.cities.firstIndex(where: {$0.id == city.id}) {
-                                                store.cities.remove(at: index)
-                                            }
-                                        } label: {
-                                            Image(systemName: "trash")
-                                        }
-                                    }
-                                    .onTapGesture {
-                                        selectedCity = city
-                                        dismiss()
-                                    }
-                            }
-                        }
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .listRowInsets(.init(top: 0, leading: 20, bottom: 5, trailing: 20))
                     }
                     
-                    .listStyle(.plain)
-                    .navigationTitle("My Cities")
-                    .navigationBarTitleDisplayMode(.inline)
-                    .preferredColorScheme(.dark)
+                    ForEach(filteredCities.sorted(using: KeyPathComparator(\.name))) { city in
+                        CityRowView(city: city)
+                            .swipeActions {
+                                Button(role: .destructive) {
+                                    if let index = store.cities.firstIndex(where: {$0.id == city.id}) {
+                                        store.cities.remove(at: index)
+                                    }
+                                } label: {
+                                    Image(systemName: "trash")
+                                }
+                            }
+                            .onTapGesture {
+                                selectedCity = city
+                                dismiss()
+                            }
+                    }
                 }
-                if isSearching {
-                    SearchOverlay(isSearching: $isSearching)
-                        .zIndex(1.0)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .listRowInsets(.init(top: 0, leading: 20, bottom: 5, trailing: 20))
+            }
+            .listStyle(.plain)
+            .navigationTitle("My Cities")
+            .navigationBarTitleDisplayMode(.inline)
+            .preferredColorScheme(.dark)
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .automatic)) {
+                ForEach(searchService.cities) { city in
+                    Button {
+                        if !store.cities.contains(where: { $0.name == city.name }) {
+                            store.cities.append(city)
+                        }
+                        searchText = ""
+                    } label: {
+                        Text("Add “\(city.name)”")
+                    }
                 }
+            }
+            .onChange(of: searchText) {
+                searchService.update(queryFragment: searchText)
             }
         }
     }
