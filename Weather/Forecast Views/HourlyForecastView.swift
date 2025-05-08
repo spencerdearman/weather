1fb9cc9b447ba533bd6cbb6aa890eb647867d55b
Spencer Dearman <dearmanspencer@gmail.com>
2025-05-08 16:03:30 -0500

    Fixing some more things with the hourslyforecast view, the spacing is off when it rains but I do not feel like fixing that right now

--- Weather/Forecast Views/HourlyForecastView.swift
+++ Weather/Forecast Views/HourlyForecastView.swift
@@ -13,34 +13,45 @@ struct HourlyForecastView: View {
     let hourlyForecast: Forecast<HourWeather>
     let timezone: TimeZone
     var body: some View {
-        Text("Hourly Forecast").font(.title)
-        Text("Next 25 hours").font(.caption)
-        ScrollView(.horizontal) {
-            HStack(spacing: 15) {
-                ForEach(hourlyForecast, id: \.date) { hour in
-                    VStack(spacing: 0) {
-                        Text(hour.date.localTime(for: timezone))
-                        Divider()
-                        Spacer()
-                        Image(systemName: hour.symbolName)
-                            .renderingMode(.original)
-                            .symbolVariant(.fill)
-                            .font(.system(size: 22))
-                            .padding(.bottom,3)
-                        if hour.precipitationChance > 0 {
-                            Text("\((hour.precipitationChance * 100).formatted(.number.precision(.fractionLength(0))))%")
-                                .foregroundStyle(.cyan)
-                                .bold()
+        VStack {
+            if let currentHour = hourlyForecast.first {
+                Text("\(currentHour.condition.description) now with a high of \(weatherManager.temperatureFormatter.string(from: currentHour.temperature))")
+                    .font(.caption).bold()
+                    .padding(.top, 10)
+                Divider()
+            }
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 22) {
+                    ForEach(hourlyForecast, id: \.date) { hour in
+                        VStack(spacing: 10) {
+                            HStack(alignment: .bottom, spacing: 0) {
+                                Text(hour.date.hourlyTime(for: timezone))
+                                    .font(.caption).bold()
+                                Text(hour.date.amPmTime(for: timezone))
+                                    .font(.caption2).bold()
+                            }
+                            Group {
+                                Image(systemName: hour.symbolName)
+                                    .renderingMode(.original)
+                                    .symbolVariant(.fill)
+                                    .font(.system(size: 22))
+                                    .padding(.bottom,3)
+                                if hour.precipitationChance > 0 {
+                                    Text("\((hour.precipitationChance * 100).formatted(.number.precision(.fractionLength(0))))%")
+                                        .foregroundStyle(.cyan)
+                                        .bold()
+                                }
+                            }
+                            .frame(maxHeight: 30)
+                            Text(removeUnits(weatherManager.temperatureFormatter.string(from: hour.temperature)))
                         }
-                        Spacer()
-                        Text(weatherManager.temperatureFormatter.string(from: hour.temperature))
                     }
                 }
+                .font(.system(size: 13))
+                .frame(height: 60)
             }
-            .font(.system(size: 13))
-            .frame(height: 100)
+            .contentMargins(.all, 20, for: .scrollContent)
         }
-        .contentMargins(.all, 15, for: .scrollContent)
         .background(RoundedRectangle(cornerRadius: 20).fill(Color.secondary.opacity(0.2)))
     }
     
